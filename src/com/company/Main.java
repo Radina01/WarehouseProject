1b26f8ee422f65b782d85e999964cd5828f0a481
Radina <radina_mihaylova0101@abv.bg>
2020-05-11 03:10:39 +0300

    new class for adding the elements to the list of array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,7 +21,8 @@ public class Main {
 
             switch (chosenOption) {
                 case 1:
-                    executeFirstFunction();
+                    AddProductsToListAndPrint addProductsToListAndPrint = new AddProductsToListAndPrint();
+                    addProductsToListAndPrint.addAndPrintProducts();
                     break;
                 case 2:
                     System.out.println();
@@ -34,13 +35,5 @@ public class Main {
                     break;
             }
         }
-    }
-    public static void executeFirstFunction() {
-        Potatoes potatoes = new Potatoes("Картофи", "Растение, зеленчук, кръгло, кафяв", "20-05-2020", "10-05-2020", "Самоковски картофи","килограми", "100", "1 секция, 3 рафт, 5 номер", "200", "Ако не завърша тази програма ще хапна един суров");
-        Tomatoes tomatoes = new Tomatoes("Домати", "Растение, плод, кръгло, червен", "17-05-2020", "09-05-2020", "Баба и Дядо ЕООД", "килограми", "50", "1 секция, 2 рафт, 4 номер", "150", "Доматите са плод и са кръгли");
-        ListOfProducts<Product> listOfProducts = new ListOfProducts<>();
-        listOfProducts.addToTable(potatoes);
-        listOfProducts.addToTable(tomatoes);
-        listOfProducts.writeToDataBase();
     }
 }
